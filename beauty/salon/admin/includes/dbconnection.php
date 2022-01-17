959bc4a215d54b5bd5d051405eb7420c92aaaed1
manaskhetan <manaskhetan@gmail.com>
2022-01-17 17:57:03 +0530

    first commit

---
+++ beauty/salon/admin/includes/dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$database="bpms";
+$con=mysqli_connect("localhost", "root", "", "bpms");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
